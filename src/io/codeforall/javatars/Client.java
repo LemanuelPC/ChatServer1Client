3e1bb4917ca8bcc0ba04444f61c25acd0ef9533a
Luis Carvalho <leprazeresc@hotmail.com>
2024-02-21 15:05:56 +0000

    Transformed into poorly made server

--- src/io/codeforall/javatars/Client.java
+++ src/io/codeforall/javatars/Client.java
@@ -9,39 +9,45 @@ public class Client {
         String serverName = args[0];
         int serverPortNumber = Integer.parseInt(args[1]);
 
-        String text = getMessage();
+        while (true) {
 
-        DatagramSocket socket = null;
-        try {
-            socket = new DatagramSocket(8000);
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
+            String text = getMessage();
+
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket(8000);
+            } catch (SocketException e) {
+                throw new RuntimeException(e);
+            }
+
+            byte[] sendBuffer = text.getBytes();
+
+            DatagramPacket sendPacket = null;
+            try {
+                sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName(serverName), serverPortNumber);
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                socket.send(sendPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            byte[] recvBuffer = new byte[1024];
+
+            DatagramPacket receivePacket = new DatagramPacket(recvBuffer, recvBuffer.length);
+            try {
+                socket.receive(receivePacket); // blocks while packet not received
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(new String(receivePacket.getData()));
+
+            socket.close();
+
         }
-
-        byte[] sendBuffer = text.getBytes();
-
-        DatagramPacket sendPacket = null;
-        try {
-            sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName(serverName), serverPortNumber);
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            socket.send(sendPacket);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        byte[] recvBuffer = new byte[1024];
-
-        DatagramPacket receivePacket = new DatagramPacket(recvBuffer, recvBuffer.length);
-        try {
-            socket.receive(receivePacket); // blocks while packet not received
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        socket.close();
 
     }
 
--- src/io/codeforall/javatars/Server.java
+++ src/io/codeforall/javatars/Server.java
@@ -9,55 +9,74 @@ public class Server {
     public static void main(String[] args) {
 
         String hostName = null;
+        int portNumber = 8080;
         try {
             hostName = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Local Server running at IP: " + hostName);
+        System.out.println("Local Server running at IP: " + hostName + ":" + portNumber);
         InetAddress serverIP;
         try {
             serverIP = InetAddress.getByName(hostName);
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-        int portNumber = 8080;
 
-        byte[] sendBuffer;
-        byte[] recvBuffer = new byte[1024];
+        InetAddress[] clients = new InetAddress[20];
+        int[] clientsPorts = new int[20];
 
-        //Receive
+        while(true) {
 
-        DatagramSocket socket = null;
-        try {
-            socket = new DatagramSocket(portNumber, serverIP);
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
+            byte[] sendBuffer;
+            byte[] recvBuffer = new byte[1024];
+
+            //Receive
+
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket(portNumber, serverIP);
+            } catch (SocketException e) {
+                throw new RuntimeException(e);
+            }
+            //System.out.println(socket.getLocalAddress().getHostAddress());
+
+            DatagramPacket receivePacket = new DatagramPacket(recvBuffer, recvBuffer.length);
+            try {
+                socket.receive(receivePacket); // blocks while packet not received
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            //Convert Data
+            String text = new String(receivePacket.getData());
+            String newText = receivePacket.getAddress().getHostAddress() + ": " + text;
+
+            for(int i = 0; i < 20; i++){
+                if(clients[i] == null){
+                    clients[i] = receivePacket.getAddress();
+                    clientsPorts[i] = receivePacket.getPort();
+                }
+            }
+
+
+            //Send
+            sendBuffer = newText.getBytes();
+            DatagramPacket sendPacket = null;
+            for (int i = 0; i < 20; i++){
+                if(clients[i] != null){
+                    sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clients[i], clientsPorts[i]);
+                }
+            }
+
+            try {
+                socket.send(sendPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            socket.close();
         }
-        //System.out.println(socket.getLocalAddress().getHostAddress());
-
-        DatagramPacket receivePacket = new DatagramPacket(recvBuffer, recvBuffer.length);
-        try {
-            socket.receive(receivePacket); // blocks while packet not received
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        //Convert Data
-        String text = new String(receivePacket.getData());
-        text = text.toUpperCase();
-
-
-        //Send
-        sendBuffer = text.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
-        try {
-            socket.send(sendPacket);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        socket.close();
 
     }
 }
